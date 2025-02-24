028a752c7b740abf1c24a9b9ddc008e56b9f5fe4
n-korel <n.korelskiy@hotmail.com>
2025-02-24 23:50:03 +0300

    fix imp the cities list

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -11,7 +11,7 @@ function CityItem({ city }) {
   const { cityName, emoji, date } = city;
 
   return (
-    <li className={styles.сityItem}>
+    <li className={styles.cityItem}>
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
       <time className={styles.date}>({formatDate(date)})</time>
