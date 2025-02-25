a1c6d08af71926ef83c25ec4fa58348dfb58cfdb
n-korel <n.korelskiy@hotmail.com>
2025-02-25 17:12:15 +0300

    React Router Building Single-Page Applications (SPA)

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,3 +1,4 @@
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -9,17 +10,29 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
   // TEMP DATA
-  const currentCity = {
+  /*   const currentCity = {
     cityName: "Lisbon",
     emoji: "🇵🇹",
     date: "2027-10-31T15:59:59.138Z",
     notes: "My favorite city so far!",
   };
 
-  const { cityName, emoji, date, notes } = currentCity;
+    const { cityName, emoji, date, notes } = currentCity; */
 
   return (
+    <>
+      <h1>City {id}</h1>
+      <p>
+        Position: {lat}, {lng}
+      </p>
+    </>
+  );
+  /*   return (
     <div className={styles.city}>
       <div className={styles.row}>
         <h6>City name</h6>
@@ -51,9 +64,9 @@ function City() {
         </a>
       </div>
 
-      <div>{/*  <ButtonBack /> */}</div>
+      <div></div>
     </div>
-  );
+  ); */
 }
 
 export default City;
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,28 @@
 import styles from "./Map.module.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const navigate = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 50, lng: 23 });
+        }}
+      >
+        Change pos
+      </button>
+    </div>
+  );
 }
 
 export default Map;
